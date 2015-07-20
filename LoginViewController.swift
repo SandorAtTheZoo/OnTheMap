7275dff56b68cd09dd66ee1445b914fb683bb3a4
SandorAtTheZoo <cjohhnso@gmail.com>
2015-07-20 11:24:18 -0400

    some optimizations and closures...commit prior to really butchering things (while it's still working)

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -27,12 +27,17 @@ class LoginViewController : UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
     @IBAction func submitLogin(sender: UIButton) {
-        //TODO: call POST method
-        nwClient.authSignIn(au.baseURL, httpBody: au.authUser(authUsername.text, pw: authPassword.text) as! String, errorStatus: authError)
-        //TODO: assign HTTPBody : trap out errors (no text in field)
-        let httpBody = au.authUser(authUsername.text, pw: authPassword.text)
+        //TODO: call POST method for login, and handle error and status as closure
+        nwClient.authSignIn(au.baseURL, httpBody: au.authUser(authUsername.text, pw: authPassword.text) as! String) {(success, errorString) in
+            if success {
+                self.completeLogin()
+            } else {
+                self.displayError(errorString)
+            }
+        }
+        //clear struct of name/pw
+        au.authUser("", pw: "")
         //TODO: assign HTTPBody with blank info after login to clear struct fields
         
     }
@@ -40,4 +45,21 @@ class LoginViewController : UIViewController {
     //TODO: create new account
     @IBAction func signUpNewAccount(sender: UIButton) {
     }
+    
+    func completeLogin() {
+        dispatch_async(dispatch_get_main_queue(), {
+            self.authError.text = ""
+            let controller = self.storyboard!.instantiateViewControllerWithIdentifier("MapNavigationController") as! UINavigationController
+            self.presentViewController(controller, animated: true, completion: nil)
+        })
+    }
+    
+    func displayError(errorString: String?) {
+        dispatch_async(dispatch_get_main_queue(), {
+            if let errorString = errorString {
+                self.authError.text = errorString
+            }
+        })
+    }
+
 }
--- OnTheMap/UserNWClient.swift
+++ OnTheMap/UserNWClient.swift
@@ -19,7 +19,7 @@ class UserNWClient : NSObject {
     //TODO: add network call functions
     //sign in to udacity
     //add closure as return to return userID
-    func authSignIn(urlString: String, httpBody : String, errorStatus : UILabel) {
+    func authSignIn(urlString: String, httpBody : String, completionHandler: (success:Bool, errorString:String?)->Void) {
         
         /* 2. Build the URL */
         let url = NSURL(string: urlString)!
@@ -40,9 +40,7 @@ class UserNWClient : NSObject {
             
             if let error = downloadError {
                 println("Could not complete the request \(error)")
-                dispatch_async(dispatch_get_main_queue()) {
-                    errorStatus.text = "Failed to connect to login server"
-                }
+                completionHandler(success: false, errorString: "Failed to connect to login server")
             } else {
                 
                 /* 5. Parse the data */
@@ -56,11 +54,19 @@ class UserNWClient : NSObject {
                         //TODO: reference closure data here for pass by reference?
                         if let user = loggedIn["key"] as? String {
                             println("user ID : \(user)")
+                            completionHandler(success: true, errorString: nil)
+                        } else {
+                            println("no user ID acquired")
+                            completionHandler(success: false, errorString: "no user ID acquired")
                         }
                         
+                    } else {
+                        println("could not find account")
+                        completionHandler(success: false, errorString: "could not find your account")
                     }
                 } else {
                     println("couldn not bring in data from udacity")
+                    completionHandler(success: false, errorString: "failed to get data from udacity")
                 }
             }
         }
@@ -69,7 +75,19 @@ class UserNWClient : NSObject {
         task.resume()
     }
     
-    
+//    /* Helper: Given raw JSON, return a usable Foundation object */
+//    class func parseJSONWithCompletionHandler(data: NSData, completionHandler: (result: AnyObject!, error: NSError?) -> Void) {
+//        
+//        var parsingError: NSError? = nil
+//        
+//        let parsedResult: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parsingError)
+//        
+//        if let error = parsingError {
+//            completionHandler(result: nil, error: error)
+//        } else {
+//            completionHandler(result: parsedResult, error: nil)
+//        }
+//    }
     
 
     class func sharedInstance() -> UserNWClient {
